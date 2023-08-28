73cc8b40a42b6d89c4702a2e39b1d09e9ec68bb7
ABertie <U2439307@rts-365.dk>
2023-08-28 12:13:16 +0200

    10 movies

---
+++ script.js
@@ -0,0 +1,62 @@
+var myMovies = [
+    {
+        title: "The Space Between Us",
+        published: 2017,
+        genre: ["Sci/fi", "Romantik"],
+        actors: ["Asa Butterfield", "Britt Robertson", "Gary Oldman"]
+    },
+    {
+        title: "Harry Potter and The philosopher's stone",
+        published: 2001,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The chamber of secrets",
+        published: 2002,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The prisoner of azkaban",
+        published: 2004,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The Goblet of Fire",
+        published: 2005,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The Order of The Phoenix",
+        published: 2007,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The Half-blood Prince",
+        published: 2009,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The Deathly Hallows: Part 1",
+        published: 2010,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Harry Potter and The Deathly Hallows: Part 2",
+        published: 2011,
+        genre: ["Fantasy"],
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+        title: "Star Wars The Last Jedi",
+        published: 2017,
+        genre: ["Sci/fi", "Action"],
+        actors: ["Daisy Ridley", "Mark Hamill", "John Boyega"]
+    }
+]
